7e71c3337de23514b5e7048ce6693d8301112890
Mick Thompson <dthompson@gmail.com>
2012-05-10 09:16:06 -0700

    add logo, random points

--- main.js
+++ main.js
@@ -17,15 +17,39 @@ $(function(){
     var map = new google.maps.Map($("#map")[0],
                                   myOptions);
 
+    $("form").submit(function(e){
+        e.preventDefault();
+        $("#search").click();
+    });
+
+    var image = 'orbotixMapPin.png';
+
     $("#search").click(function(){
         var zipcode = $("#zipcode").val();
         geocoder.geocode( { 'address': zipcode}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
+
+                for(var i =0; i<5; i++){
+                    lat = results[0].geometry.location.lat()+ (Math.random());
+                    lat = lat- (Math.random());
+                    lng = results[0].geometry.location.lng()+ (Math.random());
+                    lng = lng- (Math.random());
+                    
+                    var loc = new google.maps.LatLng(lat, lng);
+                    
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: loc,
+                        icon:image
+                    });
+                    
+
+                }
+
+
+
+
             } else {
                 alert("Geocode was not successful for the following reason: " + status);
             }
